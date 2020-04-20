4bdee2257de7c44dad1412abe20a7890b23edc9e
ginga <baba.ginga@gmail.com>
2020-04-20 23:15:52 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,215 @@
+//
+// 定数の定義
+//
+const RootName = [
+  "C", "C#", "D", "D#", "E", "F",
+  "F#", "G", "G#", "A", "A#", "B"];
+
+const Key = {
+    Maj : [0, 4, 7],
+    min : [0, 3, 7]};
+
+const Duration = {
+  "4n" : "1/4",
+  "8n" : "1/8",
+  "2n" : "1/2",
+  "1n" : "1/1"
+}
+//
+// Classの定義
+//
+class Code {
+
+  constructor(root, keyName) {　//int, [int, ...]
+    this.root = root;
+    this.keyName = keyName;
+    this.key = Key[keyName];
+    // console.log(this.root + this.key);
+    this.note = [];
+
+    for(var i = 0, l = this.key.length; i < l; i++) {
+      this.note.push(Code.scale(root+this.key[i]+keyUp));
+    }
+  }
+
+  static scale(i) {
+    var　degree = i;
+    if(i >= 12) {
+      degree -= 12;
+    }
+    var octave = 4 + parseInt((i-3)/12);
+
+    var ret = RootName[degree] + octave;
+    return ret;
+  }
+
+  name() {
+    const keyName = Object.keys(Key).filter( (key) => {
+        return Key[key] === this.key;
+    })
+    return(RootName[this.root]+keyName);
+  }
+}
+
+class Score {
+  constructor() {
+    this.list = []; //[何拍分か、何のコードか]のリスト
+  }
+
+  addNote(duration, code) {
+    console.log(duration);
+    this.list.push([duration, code]);
+  }
+
+  melodyList() {
+    var nodePos = 0;
+    var ret = [];
+    for(var i=0, l=this.list.length; i<l; i++){
+      var bars = parseInt(nodePos/4);
+      var count = nodePos - bars*4;
+      var duration = Duration[this.list[i][0]];
+      console.log(duration);
+      ret.push([
+        bars+":"+count+":0",
+        this.list[i][1].note,
+        duration]);
+      nodePos += 1;
+
+      // nodePos += parseFraction(this.list[i][0])*4;
+    }
+    return ret;
+  }
+
+  getHTML() {
+    var ret = "";
+    for(var i=0, l=this.list.length; i<l; i++){
+      ret += "<li class='note'>"
+      ret += "<span class='duration'>";
+      ret += Duration[this.list[i][0]];
+      ret += "</span>";
+      ret += "<div class='note'>";
+      ret += this.list[i][1].name();
+      ret += "</div>";
+      ret += "</li>";
+    }
+    return ret;
+  }
+}
+
+//変調をするための変数
+var keyUp = 0;
+
+var addCodeRoot = 0;
+var addCodeKeyName = "Maj";
+var duration = 1;
+
+// var duration = 1;
+console.log(duration);
+
+var score = new Score();
+var melodyList = [];  //最終的に関数に突っ込む変数
+
+var melody;
+var nodePos = 0;
+
+
+function addNote(){
+
+  //codeの取得
+  tmpCode = new Code(addCodeRoot, addCodeKeyName);
+
+  var select = document.querySelector(".duration select");
+  duration = document.querySelector(".duration select").value;
+
+  score.addNote(duration, tmpCode);
+  melodyList = score.melodyList();
+
+  melody = new Tone.Part(setPlay, melodyList).start();
+
+  //画面に表示
+  var codeList = document.getElementById("code_list");
+  var ret ="";
+  for(var i=0, l=melodyList.length; i<l; i++) {
+    ret += score.list[i][1].name();
+  }
+  codeList.innerHTML = score.getHTML();
+
+}
+
+//Initialization
+window.onload = function() {
+
+  var Am = new Code(9, "min");
+  var FM = new Code(5, "Maj");
+
+  var initialMelodys = [
+    Am, FM, Am, FM
+  ];
+
+  for(var i = 0; i < 4; i++){
+    addCodeRoot = initialMelodys[i].root;
+    addCodeKeyName = initialMelodys[i].keyName;
+    addNote();
+  }
+
+  //Playボタン再生時の処理
+  document.querySelector('#play').addEventListener('click', play);
+
+  //コードの追加処理　ルート
+  var codeButtons = document.querySelectorAll("#code_generator .root button")
+  for(var i = 0; i < codeButtons.length; i++) {
+    codeButtons[i].addEventListener('click', function() {
+
+      addCodeRoot = parseInt(this.value);
+
+      document.querySelector("#new_code .root").innerHTML = this.value;
+    });
+  }
+
+  //コードの追加処理　キー
+  var codeButtons = document.querySelectorAll("#code_generator .key button")
+  for(var i = 0; i < codeButtons.length; i++) {
+    codeButtons[i].addEventListener('click', function() {
+      console.log(this.value);
+      addCodeKeyName = this.value;
+      document.querySelector("#new_code .key").innerHTML = this.value;
+    });
+
+  }
+
+  //イベントトラッキング追加　コード追加
+  document.querySelector('#code_generator #add').addEventListener('click', addNote);
+
+}
+
+// //durationの変更コード
+// function changeDuration() {
+//   //durationの取得
+//   var select = document.querySelector(".duration select");
+//   duration = document.querySelector(".duration select").value);
+//   // console.log(duration);
+// }
+
+function play() {
+  var context = new AudioContext();
+  context.resume().then(() => {
+    console.log('Playback resumed successfully');
+  });
+
+  melody = new Tone.Part(setPlay, melodyList).start();
+  melody.loop = 4;
+  melody.loopEnd = "2m";
+  Tone.Transport.bpm.value = 90; //bpm90
+  Tone.Transport.start();
+}
+
+var synth = new Tone.PolySynth().toMaster();
+
+function setPlay(time, note) {
+  synth.triggerAttackRelease(note, '4n', time);
+}
+
+function parseFraction(f) {
+  var tmp = f.split("/");
+  return parseFloat(parseInt(tmp[0])/parseInt(tmp[1]));
+}
